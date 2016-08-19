7d5d4fb6901d90d6724fdb9be7f1d3ac9a0fa5e7
Ryan Wise <ryan.wise@legitscript.com>
2016-08-19 15:08:20 -0700

    add own heap and queue implementations
    next: use them and get rid of "algorithms" gem

---
+++ intermediate_words_search_ruby/queue.rb
@@ -0,0 +1,59 @@
+# A linked list
+# A linked list is in a valid state after a method exits if:
+#  - its length is <= 1 and @front == @back
+#  - its length is 0 and @front == nil
+#  - its length is > 1
+class Queue
+  def initialize
+    @front = nil
+    @back = nil
+  end
+
+  def empty?
+    @front.nil?
+  end
+
+  def top
+    fail if empty?
+    @front.value
+  end
+
+  def insert(value)
+    new_node = Node.new(value)
+
+    if empty?
+      @front = new_node
+      @back = new_node
+    else
+      @back.child = new_node
+      @back = new_node
+    end
+
+    nil
+  end
+
+  def pop
+    fail if empty?  # length == 0
+
+    old_top = top
+
+    if @front == @back  # length == 1
+      @front = nil
+      @back = nil
+    else  # length > 1
+      @front = @front.child
+    end
+
+    old_top
+  end
+
+  class Node
+    attr_reader :value
+    attr_accessor :child
+
+    def initialize(value)
+      @value = value
+      @child = nil
+    end
+  end
+end
