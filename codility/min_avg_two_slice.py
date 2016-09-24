41617633142df94355e5c8d0fa2e7428e8a39268
py-in-the-sky <ryanmwise1@gmail.com>
2016-09-23 17:24:31 -0700

    Update min_avg_two_slice.py

--- codility/min_avg_two_slice.py
+++ codility/min_avg_two_slice.py
@@ -14,10 +14,12 @@ def solution(A):
     best = best_ending_at = Slice((A[0] + A[1]) / 2, 2, 0)
 
     for i in xrange(2, len(A)):
-        length = best_ending_at.len + 1
-        average = (best_ending_at.avg * best_ending_at.len + A[i]) / length
-        candidate1 = Slice(average, length, best_ending_at.pos)
-        candidate2 = Slice((A[i] + A[i-1]) / 2.0, 2, i-1)
+        candidate1 = Slice((A[i] + A[i-1]) / 2, 2, i-1)
+        candidate2 = Slice(
+            (best_ending_at.avg * best_ending_at.len + A[i]) / (best_ending_at.len + 1), 
+            best_ending_at.len + 1, 
+            best_ending_at.pos
+        )
         best_ending_at = min(candidate1, candidate2)
         best = min(best, best_ending_at)
 
