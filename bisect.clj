e4267ee69b740a2df9446cde107e2dd00f80d113
py-in-the-sky <ryanmwise1@gmail.com>
2016-11-21 17:40:13 -0800

    Update bisect.clj

--- bisect.clj
+++ bisect.clj
@@ -1,3 +1,6 @@
+(defn avg [lo hi] (+ lo (/ (- hi lo) 2)))
+
+
 (defn bisect-left
   "Returns the leftmost index at which x can be inserted
   into the sorted vector v."
@@ -7,7 +10,7 @@
     ;;   - everything to the left of lo is < x
     ;;   - everything at or to the right of hi is >= x
     ;; In the end, lo == hi and lo is the leftmost spot that is >= x.
-    (let [mid (int (/ (+ lo hi) 2))]
+    (let [mid (int (avg lo hi))]
       (cond
         (= lo hi) lo
         (< (get v mid) x) (recur (inc mid) hi)
@@ -23,7 +26,7 @@
     ;;   - everything to the left of lo is <= x
     ;;   - everything at or to the right of hi is > x
     ;; In the end, lo == hi and lo is the leftmost spot that is > x.
-    (let [mid (int (/ (+ lo hi) 2))]
+    (let [mid (int (avg lo hi))]
       (cond
         (= lo hi) lo
         (> (get v mid) x) (recur lo mid)
