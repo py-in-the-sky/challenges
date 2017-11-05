f623452f1fa799465f870ef76395b5ad18e82bf0
Ryan Wise <ryanmwise1@gmail.com>
2017-11-04 22:20:04 -0700

    WIP: improving ll1_recursive_descent.py

--- arithmetic_eval/ll1_recursive_descent.py
+++ arithmetic_eval/ll1_recursive_descent.py
@@ -1,26 +1,66 @@
+"""
+Very Simple Integer-arithmetic Evaluator
+
+Two simplifications: integers are the only type of number in the inputs,
+and the exponential operation is not supported. It is left as an exercise
+to the reader to add support for floats and the exponential operation.
+
+Grammar
+
+    integer-arithmetic:  term | term plus-or-minus integer-arithmetic
+    term:                factor | factor times-or-divide term
+    factor:              integer | ( integer-arithmetic ) | unary-operator factor
+    unary-operator:      plus-or-minus
+    plus-or-minus:       + | -
+    times-or-divide:     * | /
+    integer:             \d+
+
+TODOs:
+    * nice invalid-expression error/message, as in the very simple JSON parser
+    * post as a gist
+"""
+
+
 from __future__ import division
-import re
 import operator as op
+import re
 
 
 ### Top-level
 
 def evaluate_arithmetic(arithmetic_expression):
-    return evaluate(parse(rewrite(tokenize(arithmetic_expression)))[1])
+    return compose(tokenize, parse, evaluate)(arithmetic_expression)
+    # For readability of this file, we define `evaluate_arithmetic` at the top of the file,
+    # but doing so requires us to dynamically create `compose(evaluate, parse, tokenize)`
+    # every time `evaluate_arithmetic` is called. We could define `evaluate_arithmetic` at the
+    # bottom of the file by simply doing:
+    # `evaluate_arithmetic = compose(tokenize, parse, evaluate)`.
 
 
-### Ancillary
+### Util
 
-integer_pattern = r'-?\d+'
-arithmetic_tokens = r'[()*/+-]'
-token_re = re.compile(r'(^{integer_pattern}|(?<=\D){integer_pattern}|{arithmetic_tokens})'.format(
-    integer_pattern=integer_pattern,
-    arithmetic_tokens=arithmetic_tokens
-))
+def compose(*functions_from_innermost_to_outermost):
+    def _f(x):
+        return reduce(lambda val,f: f(val), functions_from_innermost_to_outermost, x)
+
+    return _f
+
+
+### Tokenize
+
+INTEGER_PATTERN = r'\d+'
+OPERATOR_TOKENS = r'[*/+-]'
+PARENTHESES_TOKENS = r'[()]'
+TOKENS_PATTERN = r'({integer_pattern}|{operator_tokens}|{parentheses_tokens})'.format(
+    integer_pattern=INTEGER_PATTERN,
+    operator_tokens=OPERATOR_TOKENS,
+    parentheses_tokens=PARENTHESES_TOKENS
+)
+TOKEN_RE = re.compile(TOKENS_PATTERN)
 
 
 def tokenize(arithmetic_expression):
-    return (atom(s) for s in token_re.findall(arithmetic_expression))
+    return tuple(atom(s) for s in TOKEN_RE.findall(arithmetic_expression))
 
 
 def atom(string):
@@ -30,52 +70,63 @@ def atom(string):
         return string
 
 
-def rewrite(tokens):
-    return tuple(t2 for t in tokens for t2 in rewrite_rules.get(t, (t,)))
+### Parse
+
+def parse(tokens):
+    return parse_integer_arithmetic(tokens)[1]
 
 
-rewrite_rules = {
-    '-': ('+', -1, '*'),
-    '/': ('*', 1, '/')
-}
-
-
-def parse(tokens, i=0):
+def parse_integer_arithmetic(tokens, i=0, negate_first_term=False):
     i, left_term = parse_term(tokens, i)
+
+    if negate_first_term:
+        left_term = (-1, '*', left_term)
 
     if i == len(tokens) or tokens[i] == ')':
         return i, left_term
 
-    assert tokens[i] == '+'
+    assert tokens[i] in '+-'
 
-    i, remaining_expression = parse(tokens, i+1)
+    i, remaining_expression = parse_integer_arithmetic(tokens, i+1, tokens[i] == '-')
     return i, (left_term, '+', remaining_expression)
 
 
-def parse_term(tokens, i=0):
-    i, left_expression = parse_factor(tokens, i)
+def parse_term(tokens, i=0, take_reciprocal_of_first_factor=False):
+    i, left_term = parse_factor(tokens, i)
 
-    if i < len(tokens) and tokens[i] == '/':
-        i, denominator_expression = parse_factor(tokens, i+1)
-        left_expression = (left_expression, '/', denominator_expression)
+    if take_reciprocal_of_first_factor:
+        left_term = (1, '/', left_term)
 
-    if i == len(tokens) or tokens[i] in '+)':
-        return i, left_expression
+    if i == len(tokens) or tokens[i] in '+-)':
+        return i, left_term
 
-    assert tokens[i] == '*'
+    assert tokens[i] in '*/'
 
-    i, remaining_term = parse_term(tokens, i+1)
-    return i, (left_expression, '*', remaining_term)
+    i, remaining_term = parse_term(tokens, i+1, tokens[i] == '/')
+    return i, (left_term, '*', remaining_term)
 
 
 def parse_factor(tokens, i=0):
-    if tokens[i] == '(':
-        i, factor = parse(tokens, i+1)
+    if tokens[i] in tuple('+-'):
+        t = tokens[i]
+        i, factor = parse_factor(tokens, i+1)
+        return i, (1 if t == '+' else -1, '*', factor)
+    elif tokens[i] == '(':
+        i, factor = parse_integer_arithmetic(tokens, i+1)
         assert tokens[i] == ')'
         return i+1, factor
     else:
         assert isinstance(tokens[i], int)
         return i+1, tokens[i]
+
+
+### Evaluate
+
+OPERATIONS = {
+    '+': op.add,
+    '*': op.mul,
+    '/': lambda a,b: a / b  # `op.div` is not influenced by `from __future__ import division`
+}
 
 
 def evaluate(arithmetic_tree):
@@ -88,33 +139,87 @@ def evaluate(arithmetic_tree):
         return evaluate(arithmetic_tree[0])
     else:
         left_expression = evaluate(arithmetic_tree[0])
-        operation = operations[arithmetic_tree[1]]
+        operation = OPERATIONS[arithmetic_tree[1]]
         right_expression = evaluate(arithmetic_tree[2])
         return operation(left_expression, right_expression)
 
 
-operations = {
-    '+': op.add,
-    '*': op.mul,
-    '/': lambda a,b: a / b  # `op.div` is not influenced by `from __future__ import division`
-}
+### Test
+
+import random
 
 
-### Testing
+COIN = HEADS, TAILS = True, False
+BINARY_OPERATORS = '+-*/'
+UNARY_OPERATORS = '+-'
+EXPRESSION_TYPES = INT, PARENTHETIC, UNARY = ('int', 'parenthetic', 'unary')
+
+
+def generate_integer_arithmetic(recursion_depth=0):
+    expr1 = generate_expression(recursion_depth+1)
+
+    if recursion_depth < 10 and flip_coin() is HEADS:
+        binary_operator = random.choice(BINARY_OPERATORS)
+        expr2 = generate_integer_arithmetic(recursion_depth+1)
+        return ' '.join([expr1, binary_operator, expr2])
+    else:
+        return expr1
+
+
+def generate_expression(recursion_depth):
+    expression_type = random.choice(EXPRESSION_TYPES)
+
+    if expression_type is INT:
+        return generate_int()
+    elif expression_type is PARENTHETIC:
+        return '(' + generate_integer_arithmetic(recursion_depth+1) + ')'
+    else:
+        return random.choice(UNARY_OPERATORS) + generate_expression(recursion_depth+1)
+
+
+def generate_int():
+    first_digit = random.choice('123456789')
+    random_digit = lambda: random.choice('0123456789')
+    return first_digit + ''.join(random_digit() for _ in xrange(random.randint(0, 5)))
+
+
+def flip_coin():
+    return random.choice(COIN)
+
 
 def tests():
-    cases = [
+    random_cases = [generate_integer_arithmetic() for _ in xrange(1000)]
+    hard_coded_cases = [
         '4 / 2 * 3 + 4 - 6',
-        '2-2',
+        '2 * -2',
+        '2 - 2',
         '4 / 2 * (3 + 4) - 6',
-        '-23 / ((4 - 6) * (20 + 202 - 8) / 4) * 34 - 908',
+        '-23 / ((4 - 6) * (20 + 202 - 8) / 4) / 3 * 3405 * 3 / 5 / 6 / -7 - 908',
+        '-23 / ((4 - 6) * (20 + 202 - 8) / 4) / 3 * 3405 * 3 / 5 / -(-(3 + 3)) / -(3 + 4 + -10/-10) - 908',
         '(2)',
         '1/2/2',
+        '-2 + -2',
+        '(-2 + -2)',
+        '-(-2 + -2)',
+        '-(1 + 1) + -(1 + 1)',
+        '1 -1',
+        '1-1',
+        '4*3-1*5*5/5'
     ]
 
-    for case in cases:
-        print case, '=', evaluate_arithmetic(case)
-        assert evaluate_arithmetic(case) == eval(case)
+    for case in random_cases + hard_coded_cases:
+        try:
+            # print
+            # Why use `str` for the actual and expected evaluations? Due to rounding errors,
+            # my evaluator's answers diverge from Python's with very large and very small
+            # numbers. `str` rounds both large and small numbers, using scientific notation.
+            # We use `str` as a sort of rounding function.
+            actual_eval = str(evaluate_arithmetic(case))
+            expected_eval = str(eval(case))
+            # print case, '=', actual_eval, '(actual)', '=', expected_eval, '(expected)'
+            assert actual_eval == expected_eval
+        except ZeroDivisionError:
+            pass
 
     print
     print 'Tests pass!'
